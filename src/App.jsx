1d13b105c0ef68a876d186110d3f403fdb5e7414
Linus Sharp <linuscolesharp@gmail.com>
2024-05-26 19:20:50 +0100

    feat: done the me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
+import { Hero } from "./components/Me/Hero";
 import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Me/Hero.jsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I’m Linus</h1>
+        <p className={styles.description}>
+          I’m currently a student at the University of Nottingham, looking to
+          get into software development
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("Me/me.png")}
+        alt="image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
